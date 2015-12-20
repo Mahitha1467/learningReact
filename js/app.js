2668b68f3f476ce9319d6f564e227d677f654118
Murali Krishna <vmurali1444@gmail.com>
2015-12-20 14:47:19 +0530

    Initial setup of React

---
+++ js/app.js
@@ -0,0 +1,11 @@
+(function(){
+	'use strict';
+
+	let Quiz = React.createClass({
+		render: () => {
+			return <div>hello</div>;
+		}
+	});
+
+	ReactDOM.render(<Quiz />, document.getElementById("container"));
+})();
