bb27119604ccc2126cb70698f753ce10347ce906
Mahitha Arveti <marveti@thoughtworks.com>
2016-10-07 14:27:53 +0530

    [Mahitha/Nag]- web configuration- gets clicked url name

--- js/configuredSources.jsx
+++ js/configuredSources.jsx
@@ -4,6 +4,9 @@ export default class ConfiguredSources extends Component {
   render(){
     return <div id= "ConfiguredSources" className = "configured-sources">
         <span><b>My Sources</b></span>
+        <ul id = "ConfiguredSourcesList" className = "configured-sources-list">
+          
+        </ul>
     </div>
   }
 }
--- js/urls.jsx
+++ js/urls.jsx
@@ -6,9 +6,17 @@ export default class Url extends Component {
     return  <div  className = "url">
     <span className = "square-box" />
     <div className = "url-name">{this.props.title}</div>
-    <i className="fa fa-circle-thin circle" aria-hidden="true"></i>
-    <i className="fa fa-plus plus" aria-hidden="true"></i>
+    <div className = "url-click" onClick = {this._addUrl.bind(this)}>
+      <i className="fa fa-circle-thin circle" aria-hidden="true"></i>
+      <i className="fa fa-plus plus" aria-hidden="true"></i>
+    </div>
     <div className = "url-link">{this.props.link}</div>
     </div>;
+    }
+
+    _addUrl(event){
+      console.log("hello");
+      console.log($($(event.target)[0].parentNode)[0].previousSibling.innerHTML);
+
     }
 }
